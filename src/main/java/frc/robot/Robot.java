ea38226eb430f5f84526ec761544e3394e5934a6
titansrobotics <titanrobotics8174@gmail.com>
2022-03-13 21:04:37 -0600

    commented out unused RobotContainer Import

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -10,7 +10,7 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import frc.robot.RobotContainer;
+// import frc.robot.RobotContainer;
 import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.LiftTrain;
 //import edu.wpi.first.wpilibj.Encoder;
